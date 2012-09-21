6f256147877f783d48fa55f73b15c3f95e7080cc
weeska <weeska@paddi.(none)>
2012-09-21 17:25:51 +0200

    Version OpenGL

---
+++ OpenGL/glVersion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <GL/freeglut.h>
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitWindowSize(300, 300);
+	glutInitDisplayMode( GLUT_RGB );
+	glutCreateWindow("Context");
+	
+	GLchar* Names[] = {"Version", "Vendor", "GLSL Version", "Renderer"};
+	GLuint Infos[] = {GL_VERSION, GL_VENDOR, 
+			GL_SHADING_LANGUAGE_VERSION, GL_RENDERER};
+	int count = 4;
+
+	int i = 0;
+	for(i=0; i < count; i++){
+		char* version = (char*) glGetString(Infos[i]);
+
+		printf("%s: %s\n", Names[i], version);
+	}
+
+	return 0;
+}
